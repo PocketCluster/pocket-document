6ea103902a5e369184083b104aad6eab3352ef78
stkim1 <stkim1@colorfulglue.com>
2017-05-28 08:06:34 +0900

    sync fix

--- DEPSETUP/sync_node_env.sh
+++ DEPSETUP/sync_node_env.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
-rsync -rlmvt -e ssh ${HOME}/Workspace/GOPLACE/src/github.com/stkim1/pc-node-agent/* pc-node1:/opt/gopkg/src/github.com/stkim1/pc-node-agent
-rsync -rlmvt -e ssh ${HOME}/Workspace/GOPLACE/src/github.com/stkim1/pc-node-agent/* pc-node2:/opt/gopkg/src/github.com/stkim1/pc-node-agent
-rsync -rlmvt -e ssh ${HOME}/Workspace/GOPLACE/src/github.com/stkim1/pc-node-agent/* pc-node3:/opt/gopkg/src/github.com/stkim1/pc-node-agent
-rsync -rlmvt -e ssh ${HOME}/Workspace/GOPLACE/src/github.com/stkim1/pc-node-agent/* pc-node4:/opt/gopkg/src/github.com/stkim1/pc-node-agent
-rsync -rlmvt -e ssh ${HOME}/Workspace/GOPLACE/src/github.com/stkim1/pc-node-agent/* pc-node5:/opt/gopkg/src/github.com/stkim1/pc-node-agent
-rsync -rlmvt -e ssh ${HOME}/Workspace/GOPLACE/src/github.com/stkim1/pc-node-agent/* pc-node6:/opt/gopkg/src/github.com/stkim1/pc-node-agent
+find ${HOME}/Workspace/GOPLACE/ -name ".DS_Store" -depth -exec rm {} \;
+
+rsync -rlmvt --delete -e ssh ${HOME}/Workspace/GOPLACE/src/github.com/stkim1/pc-node-agent/* pc-node1:/opt/gopkg/src/github.com/stkim1/pc-node-agent
+rsync -rlmvt --delete -e ssh ${HOME}/Workspace/GOPLACE/src/github.com/stkim1/pc-node-agent/* pc-node2:/opt/gopkg/src/github.com/stkim1/pc-node-agent
+rsync -rlmvt --delete -e ssh ${HOME}/Workspace/GOPLACE/src/github.com/stkim1/pc-node-agent/* pc-node3:/opt/gopkg/src/github.com/stkim1/pc-node-agent
+rsync -rlmvt --delete -e ssh ${HOME}/Workspace/GOPLACE/src/github.com/stkim1/pc-node-agent/* pc-node4:/opt/gopkg/src/github.com/stkim1/pc-node-agent
+rsync -rlmvt --delete -e ssh ${HOME}/Workspace/GOPLACE/src/github.com/stkim1/pc-node-agent/* pc-node5:/opt/gopkg/src/github.com/stkim1/pc-node-agent
+rsync -rlmvt --delete -e ssh ${HOME}/Workspace/GOPLACE/src/github.com/stkim1/pc-node-agent/* pc-node6:/opt/gopkg/src/github.com/stkim1/pc-node-agent
