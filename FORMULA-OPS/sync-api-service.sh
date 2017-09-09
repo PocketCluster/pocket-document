9a8678dc44e376c6c817726a2d746ef487f29cdf
stkim1 <stkim1@colorfulglue.com>
2017-09-10 08:52:35 +0900

    sync command

--- FORMULA-OPS/sync-api-service.sh
+++ FORMULA-OPS/sync-api-service.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 
-BLOCKSIZE=$((1024*1024*8))
+pushd () {
+    command pushd "$@" > /dev/null
+}
+
+popd () {
+    command popd "$@" > /dev/null
+}
+
+BLOCKSIZE=$((1024 * 1024 * 8))
 COREIMAGE=${1}
 NODEIMAGE=${2}
 METAJSON=${3}
@@ -25,14 +33,16 @@ if [[ -z ${METAJSON} ]]; then
   exit
 fi
 
-echo "BlockSize ${BLOCKSIZE}, Core image : ${COREIMAGE}, Node image : ${NODEIMAGE}, Meta Json : ${METAJSON}"
-
-CORE_CHKSUM=$(pcsync build --blocksize ${BLOCKSIZE} --quite ${COREIMAGE})
-NODE_CHKSUM=$(pcsync build --blocksize ${BLOCKSIZE} --quite ${NODEIMAGE})
+CORE_CHKSUM=$(pcsync build --blocksize ${BLOCKSIZE} --quite --output-dir ../v014/package/sync/ ${COREIMAGE})
+NODE_CHKSUM=$(pcsync build --blocksize ${BLOCKSIZE} --quite --output-dir ../v014/package/sync/ ${NODEIMAGE})
 META_CHKSUM=$(pcsync meta ${METAJSON})
 PKG_VER=$(pcsync pkgver ${CORE_CHKSUM} ${NODE_CHKSUM} ${META_CHKSUM})
 
-pushd ${PWD}
+printf "BlockSize ${BLOCKSIZE}\n\tCore image : ${COREIMAGE} -> ${CORE_CHKSUM}\n\tNode image : ${NODEIMAGE} -> ${NODE_CHKSUM}\n\tMeta Json  : ${METAJSON} -> ${META_CHKSUM}\n"
+
+pcsync pkglist ${CORE_CHKSUM} ${NODE_CHKSUM} ${META_CHKSUM} ${PKG_VER} ../template/base-package.template ../v014/package/list.json
+
+pushd ${PWD} > /dev/null
 echo "syncing formula..."
 cd ../v014 && find . -name ".DS_Store" -depth -exec rm {} \; && rsync -rlmv ./* api:/api-service/v014
 popd
