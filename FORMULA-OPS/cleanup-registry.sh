5168dba4a2847b1af5193fe8eacf6c50ea16b716
stkim1 <stkim1@colorfulglue.com>
2017-10-24 17:19:57 +0900

    formula ops moved

--- FORMULA-OPS/cleanup-registry.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-source ./util.sh
-
-POCKET_REPO="${HOME}/.pocket/repository/"
-
-pushd ${PWD}
-cd ${POCKET_REPO} && rm -rf * && mkdir -p docker/registry/v2/repositories && mkdir -p docker/registry/v2/blobs
-popd
