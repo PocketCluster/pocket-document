ca909b4621d888e3a8aa520dde10b19e0ecd8127
stkim1 <stkim1@colorfulglue.com>
2017-10-28 22:32:30 +0900

    node reset script

--- DEPSETUP/reset_node_env.sh
+++ DEPSETUP/reset_node_env.sh
@@ -7,24 +7,45 @@
 
 set -ex
 SSHKEY="./sshkey/recvtest.pem"
+TARGET_HOST=${1}
+
+if [[ -z ${TARGET_HOST} ]]; then
+    echo "unable to coninue w/o target host"
+fi
 
 function reset_ssh_command() {
     HOST=${1}
     CMD=${2}
 
-    ssh -i ${SSHKEY} ${HOST} "${CMD}"
+    # ssh -i ${SSHKEY} ${HOST} "${CMD}"
+    ssh ${HOST} "${CMD}"
+}
+
+function start_pocket() {
+    echo "sudo -s service pocket start"
+}
+
+function stop_pocket() {
+    echo "sudo -s service pocket stop"
 }
 
 function cmd_reset_hostname() {
     HOST=${1}
-    echo "sudo -s rm -rf /etc/pocket/ && echo ${HOST} | sudo -s tee /etc/hostname"
+    echo "echo ${HOST} | sudo -s tee /etc/hostname"
 }
 
-reset_ssh_command pocket@192.168.1.161 "$(cmd_reset_hostname pc-node1)"
-reset_ssh_command pocket@192.168.1.162 "$(cmd_reset_hostname pc-node2)"
-reset_ssh_command pocket@192.168.1.163 "$(cmd_reset_hostname pc-node3)"
-reset_ssh_command pocket@192.168.1.164 "$(cmd_reset_hostname pc-node4)"
-reset_ssh_command pocket@192.168.1.165 "$(cmd_reset_hostname pc-node5)"
-reset_ssh_command pocket@192.168.1.166 "$(cmd_reset_hostname pc-node6)"
+function cmd_remove_pocket() {
+    echo "sudo -s rm -rf /etc/pocket"
+}
 
-rm -rf ${HOME}/.pocket/*
+function recover_cert_auth() {
+    HOST=${1}
+    scp ./sshkey/ca-certificates.crt ${HOST}:~/
+    echo "sudo -s chown root:root ca-certificates.crt && sudo -s chmod 644 ca-certificates.crt && sudo -s mv ca-certificates.crt /etc/ssl/certs/"
+}
+
+reset_ssh_command ${TARGET_HOST} "$(stop_pocket)"
+reset_ssh_command ${TARGET_HOST} "$(cmd_reset_hostname ${TARGET_HOST})"
+reset_ssh_command ${TARGET_HOST} "$(cmd_remove_pocket)"
+reset_ssh_command ${TARGET_HOST} "$(recover_cert_auth ${TARGET_HOST})"
+reset_ssh_command ${TARGET_HOST} "$(start_pocket)"
