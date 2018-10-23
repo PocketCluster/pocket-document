54d05bed16dcc3da0acaa92b869d1adc542b0554
Sung-Taek, Kim <stkim1@users.noreply.github.com>
2018-10-23 11:21:58 +0900

    VirtualHosting and SSL update (#5)
    - VirtualHosting api, index, news into one
    - Unified TLSv1.2 with various update
    
    * Unable to redirect http -> https as many `http` block confuses TLS
    * Cloudflare has an issue to connect wild card host

--- PC-INFRA/ADMIN/index/devops-seq.sh
+++ PC-INFRA/ADMIN/index/devops-seq.sh
@@ -48,3 +48,117 @@ systemctl daemon-reload
 systemctl start pocket-index
 systemctl enable pocket-index
 systemctl status pocket-index.service
+
+
+
+
+# ----------------- NGINX(#1) Let's Encrypt Wildcard Domain Cert (10/22/2018) ---------------------
+
+add-apt-repository ppa:certbot/certbot
+apt-get update
+apt-get install python-certbot-nginx
+
+# revoke existing server cert
+# https://letsencrypt.org/docs/revoking/
+certbot revoke --cert-path /etc/letsencrypt/live/index.pocketcluster.io/fullchain.pem
+
+# request wild card cert
+certbot certonly \
+--server https://acme-v02.api.letsencrypt.org/directory \
+--email stkim1@colorfulglue.com \
+-d pocketcluster.io \
+-d *.pocketcluster.io \
+--manual \
+--preferred-challenges dns-01
+
+: <<OUTPUT
+> --server https://acme-v02.api.letsencrypt.org/directory \
+> --email stkim1@colorfulglue.com \
+> -d pocketcluster.io \
+> -d *.pocketcluster.io \
+> --manual \
+> --preferred-challenges dns-01
+Saving debug log to /var/log/letsencrypt/letsencrypt.log
+Plugins selected: Authenticator manual, Installer None
+Starting new HTTPS connection (1): acme-v02.api.letsencrypt.org
+Obtaining a new certificate
+Performing the following challenges:
+dns-01 challenge for pocketcluster.io
+dns-01 challenge for pocketcluster.io
+
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+NOTE: The IP of this machine will be publicly logged as having requested this
+certificate. If you're running certbot in manual mode on a machine that is not
+your server, please ensure you're okay with that.
+
+Are you OK with your IP being logged?
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+(Y)es/(N)o: Y
+
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+Please deploy a DNS TXT record under the name
+_acme-challenge.pocketcluster.io with the following value:
+
+EMbaVoys_yALa80fgImFEwmtsUAho3YmtFMOjoW6lEg
+
+Before continuing, verify the record is deployed.
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+Press Enter to Continue
+
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+Please deploy a DNS TXT record under the name
+_acme-challenge.pocketcluster.io with the following value:
+
+_XEyhe4xMAGXy3iNXl5FWl9mg-RbtSB20dZJaCbsr6s
+
+Before continuing, verify the record is deployed.
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+Press Enter to Continue
+Waiting for verification...
+Cleaning up challenges
+
+IMPORTANT NOTES:
+ - Congratulations! Your certificate and chain have been saved at:
+   /etc/letsencrypt/live/pocketcluster.io/fullchain.pem
+   Your key file has been saved at:
+   /etc/letsencrypt/live/pocketcluster.io/privkey.pem
+   Your cert will expire on 2019-01-20. To obtain a new or tweaked
+   version of this certificate in the future, simply run certbot
+   again. To non-interactively renew *all* of your certificates, run
+   "certbot renew"
+ - If you like Certbot, please consider supporting our work by:
+
+   Donating to ISRG / Let's Encrypt:   https://letsencrypt.org/donate
+   Donating to EFF:                    https://eff.org/donate-le
+OUTPUT
+
+# ----------------- NGINX(#1) Config ---------------------
+mkdir -p /etc/nginx/ssl.crt/
+openssl dhparam -out /etc/nginx/ssl.crt/dhparams.pem 2048;
+
+mkdir -p /api-server && chown www-data:www-data /api-server && chmod 775 /api-server
+usermod -a -G www-data birdman
+
+scp default index:~/
+scp index.pocketcluster.io.conf index:~/
+scp nginx.conf index:~/
+
+mv default /etc/nginx/sites-available/
+mv index.pocketcluster.io.conf /etc/nginx/conf.d/
+mv nginx.conf /etc/nginx/
+
+chown root:root /etc/nginx/sites-available/*
+chown root:root /etc/nginx/conf.d/*
+chown root:root /etc/nginx/nginx.conf
+
+nginx -c /etc/nginx/nginx.conf -t
+service nginx restart && service nginx status
+
+
+# ------------------ FIX NGINX PID NOT FOUND --------------
+# https://www.digitalocean.com/community/questions/unable-to-start-nginx-failed-to-read-pid-from-file-run-nginx-pid
+# https://bugs.launchpad.net/ubuntu/+source/nginx/+bug/1581864
+
+mkdir /etc/systemd/system/nginx.service.d
+printf "[Service]\nExecStartPost=/bin/sleep 0.1\n" > /etc/systemd/system/nginx.service.d/override.conf
+systemctl daemon-reload
