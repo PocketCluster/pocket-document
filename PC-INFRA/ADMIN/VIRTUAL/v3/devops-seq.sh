e9d38a45a4dd39d896b59e14bd8fb057621c6d6a
Sung-Taek, Kim <stkim1@users.noreply.github.com>
2018-10-26 14:58:33 +0900

    redirect issue fix (#6)
    - fixed http -> https redirect issue
    - fixed ssl on plain http(80) issue
    - fixed in-frame news preview issue

---
+++ PC-INFRA/ADMIN/VIRTUAL/v3/devops-seq.sh
@@ -0,0 +1,23 @@
+scp * index:~/
+
+---
+
+mv default /etc/nginx/sites-available/
+mv api.pocketcluster.io.conf /etc/nginx/conf.d/
+mv index.pocketcluster.io.conf /etc/nginx/conf.d/
+mv news.pocketcluster.io.conf /etc/nginx/conf.d/
+mv nginx.conf /etc/nginx/
+rm devops-seq.sh
+
+chown root:root /etc/nginx/sites-available/*
+chown root:root /etc/nginx/conf.d/*
+chown root:root /etc/nginx/nginx.conf
+
+nginx -c /etc/nginx/nginx.conf -t
+
+service nginx restart && service nginx status
+
+---
+
+curl -iv https://api.pocketcluster.io/service/v014/package/list
+curl -iv https://api.pocketcluster.io/healthcheck
